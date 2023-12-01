7f4a3ac513c920fa815faf05413ba321a0babc12
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-12-01 15:16:30 +0100

    fixed compress and extract

--- src/extract.h
+++ src/extract.h
@@ -6,6 +6,8 @@
 #define AD3_PROJECT_EXTRACT_H
 
 #include "tree/linked_list.h"
+#include <stdio.h>
+
 
 int extract(char *inputFile, char *outputFile, int bufferSize);
 node* readHeader(FILE *input);
